a4e20d8cdd6a0db2bb6cc6c77155357b6a53ae16
hackerFaruk <onuralparslan97@gmail.com>
2025-02-09 11:57:08 +0300

    better data set creators

--- 8-Generative/imgs.py
+++ 8-Generative/imgs.py
@@ -5,13 +5,21 @@ import numpy as np
 
 folder=Path("Images")
 counter=0
+modeTanh=True
+
 
 for entry in folder.iterdir():
     for png in entry.iterdir():
         img=cv2.imread(png, cv2.IMREAD_GRAYSCALE)
         img=cv2.resize(img,(128,128))
-        img=img/255.0 # normalize
+        img=img/255.0 # normalize to 0,1 good for sigmoid
+        if modeTanh:
+             img = (img * 2) - 1 #scale to tanh 
+
         counter+=1
-        cv2.imwrite(f"database/{counter}.png", (img * 255).astype(np.uint8))
+        if modeTanh:
+            cv2.imwrite(f"database/{counter}.png", ((img + 1) * 127.5).astype(np.uint8))  # Convert back to [0, 255] for saving
+        else:
+            cv2.imwrite(f"database/{counter}.png", (img * 255).astype(np.uint8))
 
         
